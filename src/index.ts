cbae1d97121351159040994434ca1afc71699222
martinsllm <lucaslm2010@gmail.com>
2024-09-07 11:54:52 -0300

    Adjustment in fastify server

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import 'dotenv/config';
 
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
 
-server.listen({ port: +PORT }, (err, address) => {
+server.listen({ port: +PORT, host: "0.0.0.0" }, (err, address) => {
     console.log(`Server listening on ${address}`);
 });
